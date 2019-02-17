962fe4aefe898702b571f01afc40d4e3fa058421
Fernando Gama da Mata <f.fabiogama88@yahoo.com.br>
2019-02-17 14:11:20 -0300

    Scripts (Python / R)

--- scripts/csv_to_sqlite/Moving_data_csv_sqlite.R
+++ scripts/csv_to_sqlite/Moving_data_csv_sqlite.R
@@ -1,18 +1,22 @@
 #  movendo dados de um CSV para um SQLite
 setwd("sakila_data/")
-library(dplyr)
+
 library(readr)
 
+# buscar todos os arquivos csv do diretório
 full_dataset <- lapply(list.files(pattern="*.csv"), read_csv)
 
 library(RSQLite)
 
+# driver de conexão
 drv <- dbDriver("SQLite")
 
 con <- dbConnect(drv, dbname = "r_database_films.db")
 
-table_names <- c("actor", "film", "film_actor", "language")
+# nomes dos arquivos
+table_names <- c(gsub("[.]csv", "", dir(pattern = "*.csv")))
 
+# para cada linha do dataset faça a inserção no banco
 for (row in seq_along(full_dataset)){
   dbWriteTable(con, table_names[row], full_dataset[[row]], row.names = FALSE) 
 }
